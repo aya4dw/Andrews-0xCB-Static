e37b20f221cceb1a04d874b970f7b8cd086021ab
Andrew <andrew@company.com>
2026-05-27 13:32:43 +0800

    Initial import: 0xCB Static Vial keymap with Bongocat OLED animation

---
+++ keymaps/vial/config.h
@@ -0,0 +1,5 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#pragma once
+#define VIAL_KEYBOARD_UID {0x56, 0xB3, 0x00, 0x5D, 0x6F, 0xE1, 0xAA, 0xA7}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
+#define VIAL_UNLOCK_COMBO_COLS {0, 0}
